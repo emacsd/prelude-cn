91a4bbc908e3e948e1d482bd05c8b65dfc69435e
Luo Tao <lotreal@gmail.com>
2018-02-06 11:34:32 +0800

    fix git-bash

--- personal/windows.el
+++ personal/windows.el
@@ -1,11 +1,7 @@
 ;; Set Windows-specific preferences if running in a Windows environment.
 (defun udf-windows-setup () (interactive)
-       ;; The variable `git-shell-path' contains the path to the `Git\bin'
-       ;; file on my system. I install this in
-       ;; `%USERPROFILE%\LocalAppInfo\apps\Git\bin'.
-       (setq git-shell-path "C:/Program Files/Git/bin/")
-       (setq git-shell-executable
-             (concat git-shell-path "\\bash.exe"))
+       (setq git-shell-path "C:\\Program Files\\Git\\bin")
+       (setq explicit-shell-file-name (concat git-shell-path "\\bash.exe"))
        (add-to-list 'exec-path git-shell-path)
        (setenv "PATH"
                (concat git-shell-path ";"
